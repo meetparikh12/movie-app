e8f9a39ad89c703067b3384bf177575458f264a9
meetparikh12 <parikhmeet1234@gmail.com>
2020-07-23 21:25:56 +0530

    Modified navbar component

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,26 +1,65 @@
 import React, { Fragment } from 'react'
-import { AppBar, Toolbar, Button, IconButton} from '@material-ui/core'
+import { AppBar, Toolbar, Button, IconButton, Menu, MenuItem} from '@material-ui/core'
 import Home from '@material-ui/icons/Home'
 import MenuIcon from '@material-ui/icons/Menu'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [width] = React.useState(document.documentElement.clientWidth)
     
+    const buttonsToShow = (
+        <Fragment>
+            <Button color="inherit" component={Link} to="/"><Home/></Button>
+            <Button color="inherit" component={Link} to="/movie/now-playing">NOW PLAYING</Button>
+            <Button color="inherit" component={Link} to="/movie/upcoming">UPCOMING</Button>
+            <Button color="inherit" component={Link} to="/movie/latest">LATEST</Button>
+        </Fragment>
+    )
+    
+    const handleClick = (event)=> {
+        setAnchorEl(event.currentTarget)
+    }
+    const handleClose = ()=> {
+        setAnchorEl(null)
+    }
+    let contentToShow;
+
+    if(width>500){
+        contentToShow = (
+            <Fragment>
+                {buttonsToShow}
+            </Fragment>
+        )
+    }else if(width<=500){
+        contentToShow = (
+        <Fragment>
+            <IconButton onClick={handleClick} aria-controls="simple-menu" edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon />
+            </IconButton>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose} component={Link} to="/">Home</MenuItem>
+                <MenuItem onClick={handleClose} component={Link} to="/movie/now-playing">Now Playing</MenuItem>
+                <MenuItem onClick={handleClose} component={Link} to="/movie/upcoming">Up Coming</MenuItem>
+                <MenuItem onClick={handleClose} component={Link} to="/movie/latest">Latest</MenuItem>
+            </Menu>
+        </Fragment>  
+        )             
+    }
     return (
         <AppBar>
             <Toolbar className="nav-container">
-                <IconButton edge="start" color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <Fragment>
-                    <Button color="inherit" component={Link} to="/"><Home/></Button>
-                    <Button color="inherit" component={Link} to="/movie/now-playing">NOW PLAYING</Button>
-                    <Button color="inherit" component={Link} to="/movie/upcoming">UPCOMING</Button>
-                    <Button color="inherit" component={Link} to="/movie/latest">LATEST</Button>
-                </Fragment>
+                {contentToShow}
             </Toolbar>
         </AppBar>
     )
+   
 }
 
 export default Navbar;
